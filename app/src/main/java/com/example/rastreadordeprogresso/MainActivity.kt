123a2bda485c9c492522b45d88041a357fb4fd14
marquisalexander <marquis.alexander@outlook.com>
2024-04-29 22:16:02 -0300

    feature: add foods options

--- app/src/main/java/com/example/rastreadordeprogresso/MainActivity.kt
+++ app/src/main/java/com/example/rastreadordeprogresso/MainActivity.kt
@@ -12,8 +12,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -26,11 +28,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.rastreadordeprogresso.ui.theme.Background
 import com.example.rastreadordeprogresso.ui.theme.Blue
+import com.example.rastreadordeprogresso.ui.theme.Description
 import com.example.rastreadordeprogresso.ui.theme.RastreadorDeProgressoTheme
+import com.example.rastreadordeprogresso.ui.theme.Title
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,25 +66,25 @@ fun Greeting() {
 
 @Composable
 fun View() {
+    val obj = arrayOf("Snack", "Pasta", "Main")
     Column(
         modifier = Modifier
             .background(Background)
     ) {
         Row(
             modifier = Modifier
-                .padding(0.dp, 16.dp)
+                .padding(16.dp, 16.dp)
                 .horizontalScroll(rememberScrollState())
         ) {
-            CustomCard("Snack", 1)
-            CustomCard("Main", 2)
-            CustomCard("Pasta", 3)
-            CustomCard("Pasta", 4)
-            CustomCard("Pasta", 5)
-            CustomCard("Pasta", 6)
-            CustomCard("Pasta", 7)
+            CustomCard(obj.get(0), 0)
+            CustomCard(obj.get(1), 1)
+            CustomCard(obj.get(2), 2)
+            CustomCard(obj.get(2), 2)
+            CustomCard(obj.get(2), 2)
+            CustomCard(obj.get(2), 2)
         }
         Column(modifier = Modifier.padding(16.dp)) {
-            MainCard()
+            MainCard(obj.get(1))
         }
     }
 
@@ -86,45 +92,84 @@ fun View() {
 
 @Composable
 fun CustomCard(title: String, index: Int) {
-    var paddingLeft = if (index == 1) 16 else 5
+    var isFirst = if (index == 0) 0 else 6
     Card(
         colors = CardDefaults.cardColors(
             containerColor = Blue
         ),
         modifier = Modifier
-            .size(
-                width = 110.dp, height = 110.dp
-            )
-            .padding(paddingLeft.dp, 0.dp, 0.dp, 0.dp)
+            .heightIn(0.dp, 100.dp)
+            .widthIn(0.dp, 100.dp)
+            .padding(isFirst.dp, 0.dp, 0.dp, 0.dp)
     ) {
+        val monthString = when (title) {
+            "Snack" -> R.drawable.hamburger
+            "Pasta" -> R.drawable.pasta
+            "Main" -> R.drawable.main
+            else -> R.drawable.hamburger
+        }
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                painter = painterResource(id = monthString),
                 contentDescription = "food",
-                modifier = Modifier.size(50.dp)
+                modifier = Modifier.size(70.dp)
             )
-            Text(text = title)
+            Text(
+                text = title,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium,
+                color = Title
+            )
         }
     }
 }
 
 @Composable
-fun MainCard() {
-    Column(
-        modifier = Modifier.background(Blue).fillMaxWidth(),
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = "food",
-            modifier = Modifier.height(200.dp).fillMaxWidth()
+fun MainCard(title: String) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = Blue
         )
-        Text(text = "Spaghetti Bolognese")
-        Text(text = "Spaghetti paste with Italian-style minced meat sauce.")
-        Text(text = "$ 19.99")
+    ) {
+        val monthString = when (title) {
+            "Snack" -> R.drawable.hamburger
+            "Pasta" -> R.drawable.pasta
+            else -> R.drawable.hamburger
+        }
+        Image(
+            painter = painterResource(id = monthString),
+            contentDescription = "food",
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+        )
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Spaghetti Bolognese",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium,
+                color = Title
+            )
+            Text(
+                text = "Spaghetti paste with Italian-style minced meat sauce.",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Light,
+                color = Description
+            )
+            Text(
+                text = "$ 19.99",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Description
+            )
+        }
     }
 
 }
